d296d447769de8257cfa31b1675e93031578d7c1
Junho Choi <tig9659@ajou.ac.kr>
2023-03-19 01:56:23 +0900

    글 작성 연결

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,13 +1,7 @@
 export default function Modal({ show, handleModal, status}) {
-  /*
-          <div style={{textAlign:"right", marginTop:"1rem"}}>
-            <button type="button">
-              신청하기
-            </button>
-          </div>
-          */
-    return (
+    const handleClick = () => alert("신청이 완료되었습니다.");
 
+    return (
       <div className={"modal-wrap " + (show ? "active" : "")}>
         <div className="overlay" onClick={() => {handleModal(false);}} style={{
             position: "absolute",
@@ -34,7 +28,7 @@ export default function Modal({ show, handleModal, status}) {
             <div>
                 <div style={{fontSize:"1.5rem", marginBottom:"0.6rem"}}>식사메이트</div>
                 <div style={{border:"1px solid gray", borderRadius: "2rem", height:"6rem"}}>
-                    <div style={{display:"flex"/*, justifyContent: "space-around"*/}}>
+                    <div style={{display:"flex"}}>
                         <div style={{width:"5rem", borderRight:"1px solid gray", padding:"0 1rem 0 2rem", marginTop:"0.2rem"}}>4인</div>
                         <div style={{width:"5rem", borderRight:"1px solid gray", padding:"0 1rem", marginTop:"0.2rem"}}>무관</div>
                         <div style={{width:"9rem",  padding:"0 1rem ", borderRight:"1px solid gray"}}>03/18 12:00</div>
@@ -50,61 +44,16 @@ export default function Modal({ show, handleModal, status}) {
             </div>
             {status === "MYMATE" ? <></> :
               (status === "APPLY" ? <div style={{textAlign:"right", marginTop:"1rem"}}>
-                <button type="button" style={{background:"5F95E5"}}>
+                <button type="button" style={{background:"5F95E5"}} onClick={handleClick}>
                   신청하기
                 </button>
               </div> : <div style={{textAlign:"right", marginTop:"1rem"}}>
-                <button type="button" style={{background:"5F95E5"}}>
+                <button type="button" style={{background:"5F95E5"}} onClick={handleClick}>
                   모집하기
                 </button>
               </div>)
               }
         </div>
       </div>
-
-/*
-     <div style={{
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        position: "fixed",
-        left: 0,
-        top: 0,
-        textAlign: "center",
-     }}>
-        <div style={{
-            height: "100%",
-            width: "950px",
-            marginTop: "70px",
-            position: "relative",
-            overlow: "scroll",
-            background: "#141414",
-        }}>
-        <div>
-                <div>식사메이트</div>
-                <div style={{border:"1px solid gray"}}>
-                    <div>4인</div>
-                    <div>무관</div>
-                    <div>03/18</div>
-                    <div>12:00</div>
-                    <div>구학생회관</div>
-                    <div>학생식당에서 점심 같이 드실 분</div>
-                    <div>식당 앞에서 만나서 들어가요</div>
-                </div>
-            </div>
-            <div>
-                <div>코멘트</div>
-                <div>같이 어떤 메뉴 먹어요~</div>
-            </div>
-          <div className="bottom">
-            <button type="button">
-              신청하기
-            </button>
-          </div>
-        </div>
-     </div>
-*/
     );
   }
